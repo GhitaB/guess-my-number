f349b9cecddb3e59d10a02061da2f9f11b63e5a1
Ghiță Bizău <ghitabizau27@gmail.com>
2014-12-18 22:26:52 +0200

    Random int number and some updates.

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-	var choose_number = function() {
-		a_number = 4321;
+	function get_random_int_number(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function choose_number() {
+		a_number = get_random_int_number(1234, 9876);
 		return a_number;
 	}
 
-	var show_status = function(text_bold, text_normal) {
+	function show_status(text_bold, text_normal) {
 		$("#status").html("<b>" + text_bold + "</b> " + text_normal);
 	}
 
-	var start_game = function() {
+	function start_game() {
 		var number = choose_number();
 		$("#choose_number").hide();
 		show_status("Status:", "Game started.");
