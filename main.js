c69292ff3228561f97b16ae9724a2848c8a73258
Ghiță Bizău <ghitabizau27@gmail.com>
2014-12-19 06:57:41 +0200

    Updates. The number has distict digits now.

--- main.js
+++ main.js
@@ -3,8 +3,26 @@ $(document).ready(function(){
     	return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
+	function number_has_distinct_digits(a_number) {
+		d4 = a_number % 10;
+		d3 = parseInt(a_number / 10) % 10;
+		d2 = parseInt(a_number / 100) % 10;
+		d1 = parseInt(a_number / 1000) % 10;
+
+		if(d1 != d2 && d1 != d3 && d1 != d4 && d2 != d3 && d2 != d4 && d3 != d4) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	function choose_number() {
-		a_number = get_random_int_number(1234, 9876);
+		while(true) {
+			a_number = get_random_int_number(1234, 9876);
+			if(number_has_distinct_digits(a_number)) {
+				break;
+			}
+		}
 		return a_number;
 	}
 
@@ -19,5 +37,6 @@ $(document).ready(function(){
 		console.log(number);
 	}
 
+	show_status("Status:", "Are you ready? Press the button to start.");
 	$("#choose_number").on("click", start_game);
 });
